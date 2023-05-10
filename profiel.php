6e1b7a456906a1013b3128b155569041ad62cae0
Lore Marissens <lore.marissens@gmail.com>
2023-05-10 10:42:38 +0200

    added profiel.php

---
+++ profiel.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css"/>
+    <title>profiel</title>
+</head>
+<body>
+    <div class="info-profiel">
+        <h2>Voornaam<h2>
+        <input type="email" id="email" name="email" value="Julie" required />
+        <h2>Achternaam<h2>
+        <input type="email" id="email" name="email" value="De Vos" required />
+        <h2>Email adres<h2>
+        <input type="email" id="email" name="email" value="julie.devos@gmail.com" required />
+        <h2>Wachtwoord<h2>
+        <input type="email" id="email" name="email" value="Kippenbout2023" required />
+    </div>
+
+    <div class="hexagon">
+        <img src="./media/Julie.png" alt="Photo" class="hex-img">
+    </div>
+
+    <button type="submit" class="btn">Bewerken</button>
+
+</body>
+</html>
