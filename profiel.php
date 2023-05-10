c12d3b431938e82a615aafbb7332af073adece65
Zahra Janssens <zzahra.janssens@telenet.be>
2023-05-10 11:30:20 +0200

    add the nav

--- profiel.php
+++ profiel.php
@@ -8,6 +8,28 @@
     <title>profiel</title>
 </head>
 <body>
+  
+    <nav class="search">
+        <div id="menu">
+        <a href="search.php">Zoeken</a>
+        </div>
+    </nav>
+
+    <div class="image-container">
+        <img class="row" src="./media/Blauwe-balk.png" alt="blauwe balk">
+        <img class="logo-wit" src="./media/logo-wit.png" alt="Logo Conneco">
+    </div>
+
+    <div class="nav-container">
+        <a href="dashboard.php"><h2>Dashboard</h2></a> 
+        <a href="werknemers.php"><h2>Werknemers</h2></a>
+        <a href="categorieën.php"><h2>Categorieën</h2></a>
+        <a href="Badges.php"><h2>Badges</h2></a>
+        <a href="Profiel.php"><h2>Profiel</h2></a>
+        <a href="Instellingen.php"><h2>Instellingen</h2></a>
+    </div>
+
+
     <div class="info-profiel">
         <h2>Voornaam<h2>
         <input type="email" id="email" name="email" value="Julie" required />
