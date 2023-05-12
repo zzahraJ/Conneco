0d82d09af99f8d75d565da7c38fac2e0f542aa45
Zahra Janssens <zzahra.janssens@telenet.be>
2023-05-12 11:50:00 +0200

    feat: added the instellingen.php

--- instellingen.php
+++ instellingen.php
@@ -9,27 +9,52 @@
 </head>
     <body>
         
-         <nav class="search">
+        <nav class="search">
             <div id="menu">
                 <a href="search.php">Zoeken</a>
                 <input type="text" name="search" placeholder="Zoeken">
             </div>
         </nav>
 
-    <div class="image-container">
-        <img class="row" src="./media/Blauwe-balk.png" alt="blauwe balk">
-        <img class="logo-wit" src="./media/logo-wit.png" alt="Logo Conneco">
-    </div>
+        <div class="image-container">
+            <img class="row" src="./media/Blauwe-balk.png" alt="blauwe balk">
+            <img class="logo-wit" src="./media/logo-wit.png" alt="Logo Conneco">
+        </div>
 
-    <div class="nav-container">
-        <a href="dashboard.php"><P>Dashboard</P></a> 
-        <a href="werknemers.php"><p>Werknemers</p></a>
-        <a href="categorieën.php"><p>Categorieën</p></a>
-        <a href="badges.php"><p>Badges</p></a>
-        <a href="profiel.php"><p>Profiel</p></a>
-        <a href="instellingen.php"><p>Instellingen</p></a>
-    </div>
-   
+        <div class="nav-container">
+            <a href="dashboard.php"><P>Dashboard</P></a> 
+            <a href="werknemers.php"><p>Werknemers</p></a>
+            <a href="categorieën.php"><p>Categorieën</p></a>
+            <a href="badges.php"><p>Badges</p></a>
+            <a href="profiel.php"><p>Profiel</p></a>
+            <a href="instellingen.php"><p>Instellingen</p></a>
+        </div>
     
+        <div class="omgedraaid-vormpje">
+            <img src="./media/vorm-omgedraaid.png" alt="vormpje">
+        </div>
+
+        <div class="instellingen">
+          <label for="firstname">Voornaam</label>
+          <input type="text" id="firstname" name="firstname" value="" required />
+
+          <label for="lastname">Achternaam</label>
+          <input type="text" id="lastname" name="lastname" value="" required />
+
+          <label for="email">Email</label>
+          <input type="email" id="email" name="email" value="" required />
+
+          <label for="password">Wachtwoord</label>
+          <input type="password" id="password" name="password" required />
+        </div>
+
+        <div class="bewerken"><a href="instellingenBewerken.php">Bewerken</a></div>
+        <!-- <button type="submit" class="btn">Bewerken</button> -->
+
+
+
+
+
+
     </body>
 </html>
