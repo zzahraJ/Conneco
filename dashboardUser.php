094a511731b52dc8b5cd86d340197ce8e431570f
Zahra Janssens <zzahra.janssens@telenet.be>
2023-05-18 11:38:48 +0200

    feature: changed the navbar

---
+++ dashboardUser.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css"/>
+    <title>Dashboard</title>
+</head>
+<body>
+
+        <nav class="search">
+            <div id="menu">
+                <a href="search.php">Zoeken</a>
+                <input type="text" name="search" placeholder="Zoeken">
+            </div>
+        </nav>
+
+    <div class="image-container">
+        <img class="row" src="./media/Blauwe-balk.png" alt="blauwe balk">
+        <img class="logo-wit" src="./media/logo-wit.png" alt="Logo Conneco">
+         
+    </div>
+    
+    <div class="nav-container">
+        <a href="dashboardUser.php"><P>Dashboard</P></a> 
+        <a href="chat.php"><p>Chat</p></a>
+        <a href="profielUser.php"><p>Profiel</p></a>
+        <a href="badgesUser.php"><p>Badges</p></a>
+        <a href="instellingenUser.php"><p>Instellingen</p></a>
+    </div>
+
+    <div class="lopende-projecten">
+            <h2>Lopende Projecten</h2>
+        </div>
+
+
+    <div class="eigen-projecten">
+        <h2>Eigen Projecten</h2>
+    </div>
+
+    <!-- hier moet php code in om de naam van het project weer te geven -->
+    <div class="projecten">
+        <div class="project"> 
+            <h2>Project naam:</h2>
+            <p>Geld te beschikking stellen voor klanten</p>  <!-- hier moet je php code schrijven -->
+            <h2>Werknemers die hieraan werken:</h2>
+            <p>Thomas Peeters    Julie De Vos    Mark Mezel</p> <!-- hier moet je php code schrijven -->
+        </div>
+
+        <div class="project"> 
+            <h2>Project naam:</h2>
+            <p>Geld te beschikking stellen voor klanten</p>  <!-- hier moet je php code schrijven -->
+            <h2>Werknemers die hieraan werken:</h2>
+            <p>Thomas Peeters    Julie De Vos    Mark Mezel</p> <!-- hier moet je php code schrijven -->
+        </div>
+    
+
+        <div class="project"> 
+            <h2>Project naam:</h2>
+            <p>Geld te beschikking stellen voor klanten</p>  <!-- hier moet je php code schrijven -->
+            <h2>Werknemers die hieraan werken:</h2>
+            <p>Thomas Peeters    Julie De Vos    Mark Mezel</p> <!-- hier moet je php code schrijven -->
+        </div>
+    
+
+        <div class="project"> 
+            <h2>Project naam:</h2>
+            <p>Geld te beschikking stellen voor klanten</p>  <!-- hier moet je php code schrijven -->
+            <h2>Werknemers die hieraan werken:</h2>
+            <p>Thomas Peeters    Julie De Vos    Mark Mezel</p> <!-- hier moet je php code schrijven -->
+        </div>
+    </div>
+   
+    <div class="deadlines">
+        <h2>Volgende Deadline</h2>
+        <h2>Werknemers die hieraan werken</h2>
+    </div>
+    
+</body>
+</html>
