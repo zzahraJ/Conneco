8cfe703de8ab4ff6c42b8dc6ce07c090343a9960
Zahra Janssens <zzahra.janssens@telenet.be>
2023-05-18 13:56:32 +0200

    feature: added the labels for the profile

--- profielUser.php
+++ profielUser.php
@@ -21,31 +21,65 @@
         <img class="logo-wit" src="./media/logo-wit.png" alt="Logo Conneco">
     </div>
 
-    <div class="nav-container">
-        <a href="dashboardUser.php"><P>Dashboard</P></a> 
-        <a href="chat.php"><p>Chat</p></a>
-        <a href="profielUser.php"><p>Profiel</p></a>
-        <a href="badgesUser.php"><p>Badges</p></a>
-        <a href="instellingenUser.php"><p>Instellingen</p></a>
-    </div>
-   
+        <div class="nav-container">
+            <a href="dashboardUser.php"><P>Dashboard</P></a> 
+            <a href="chat.php"><p>Chat</p></a>
+            <a href="profielUser.php"><p>Profiel</p></a>
+            <a href="badgesUser.php"><p>Badges</p></a>
+            <a href="instellingenUser.php"><p>Instellingen</p></a>
+        </div>
 
-    <div class="info-profiel">
+        <div class="profiel">
+          <label for="naam">naam</label>
+          <input type="text" id="naam" name="naam" value="Julie" required />
+
+          <label for="functie">Functie</label>
+          <input type="text" id="functie" name="functie" value="De Vos" required />
+
+          <label for="afdeling">Afdeling</label>
+          <input type="text" id="afdeling" name="afdeling" value="Q7 bureau 5" required />
+
+          <label for="inDienst">In dienst sinds</label>
+          <input type="text" id="inDienst" name="inDienst" value="Werknemers sinds 2019" required />
+
+          <label for="bio">Bio</label>
+          <input type="text" id="bio" name="bio" value="Een vrolijke 27-jarige" required />
+        </div>
+
+        <div class="profielfoto"><img src="./media/julie.png" alt="profielfoto"></div>
+        <div class="bewerken"><a href="profielBewerkenUser.php">Bewerken</a></div>
+        <div class="uitloggen"><a href="logout.php">Uitloggen</a></div>
+        <!-- <button type="submit" class="btn">Bewerken</button> -->
+
+        <!-- <div class="profiel">
+          <label for="naam">Naam</label>
+          <input type="text" id="naam" name="naam" value="Julie De Vos" required />
+
+          <label for="functie">Functie</label>
+          <input type="text" id="functie" name="functie" value="Administratied Medewerker" required />
+
+          <label for="afdeling">Afdeling</label>
+          <input type="text" id="afdeling" name="afdeling" value="Q7 bureau 5" required />
+
+          <label for="InDienst">In dienst sinds</label>
+          <input type="text" id="inDienst" name="inDienst" value="Werknemer sinds 2019" required />
+
+          <label for="Bio">Bio</label>
+          <input type="text" id="bio" name="bio" value="Een vrolijke 27-jarige." required />
+
+        </div> -->
+
+    
+    <!-- <div class="info-profiel">
         <h2>Voornaam<h2>
-        <input type="email" id="email" name="voornaam" value="Julie" required />
+        <input type="email" id="email" name="email" value="Julie" required />
         <h2>Achternaam<h2>
-        <input type="email" id="email" name="achternaam" value="De Vos" required />
+        <input type="email" id="email" name="email" value="De Vos" required />
         <h2>Email adres<h2>
         <input type="email" id="email" name="email" value="julie.devos@gmail.com" required />
         <h2>Wachtwoord<h2>
-        <input type="email" id="email" name="wachtwoord" value="Kippenbout2023" required />
-    </div>
-
-    <div class="hexagon">
-        <img src="./media/Julie.png" alt="Photo" class="hex-img">
-    </div>
-
-    <button type="submit" class="btn">Bewerken</button>
+        <input type="email" id="email" name="email" value="Kippenbout2023" required />
+    </div> -->
 
 </body>
 </html>
