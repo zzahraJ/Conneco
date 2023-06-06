2826333e5a9c7349666d5a8265044c575e9333ea
Zahra Janssens <zzahra.janssens@telenet.be>
2023-06-06 17:39:37 +0200

    feature: now you can login as a user

--- login.php
+++ login.php
@@ -1,4 +1,87 @@
-<!DOCTYPE html>
+<?php
+    function canLogin($username, $password){
+        if($username == "test0@thomasmore.be" && $password == "12345"){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    if(!empty($_POST)){
+      $username = $_POST['username'];
+      $password = $_POST['password'];
+
+      if(canLogin($username, $password)){
+        session_start();
+        $_SESSION['username'] = $username;
+        header('Location: dashboardUser.php');
+       
+      }
+      else {
+        $error = "Username or password is incorrect";
+      }
+
+    }
+?>
+
+<header>
+<link rel="stylesheet" href="style.css"/>
+  <nav class="nav">
+    <a href="?page=browser">Browse</a>
+      <a href="?page=getdesktop">Get desktop</a>
+      <a href="?page=tryprime">Try prime</a>
+    <a href="#" class="loggedIn">
+      <div class="user--avatar"><img src="https://images.unsplash.com/photo-1502980426475-b83966705988?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=ddcb7ec744fc63472f2d9e19362aa387" alt=""></div>
+      <h3 class="user--name">Username here</h3>
+      <span class="user--status">Watching dakotaz</span>
+    </a>
+    <a href="logout">Log out?</a>
+  </nav>    
+</header>
+
+<div id="app">
+    <form action="login.php" method="post">
+    
+        <h1>Log in to Conneco</h1>
+        <nav class="nav--login">
+            <a href="#" id="tabLogin">Log in</a>
+            <a href="#" id="tabSignIn">Sign up</a>
+        </nav>
+    
+        <?php if(isset($error)): ?>
+          <div class="alert">That password was incorrect. Please try again</div>
+        <?php endif; ?>
+    
+        <div class="form form--login">
+            <label for="username">Username</label>
+            <input type="text" id="username" name="username">
+        
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password">
+        </div>
+        
+        <div class="form form--signup hidden">
+            <label for="username2">Username</label>
+            <input type="text" id="username2">
+        
+            <label for="password2">Password</label>
+            <input type="password" id="password2">
+            
+            <label for="email">Email</label>
+            <input type="text" id="email">
+        </div>
+    
+        <input type="submit" value="Log In" class="btn">
+        <!-- <a href="#" class="btn" id="btnSubmit">Log In</a> -->
+     </form>
+</div>
+
+
+
+
+
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -12,7 +95,7 @@
     
     <img class="logo" src="./media/oceaanblauw.png" alt="Logo Conneco">
 
-    <form class="form-login" method="post" > <!-- Form for logging in / HIER STOND EEN ACTION MET PHP CODE -->
+    <form class="form-login" method="post" > <!- Form for logging in / HIER STOND EEN ACTION MET PHP CODE -
        
         <div class="input-login">
           <label for="email">Email</label>
