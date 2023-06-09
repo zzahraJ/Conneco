01e2157fac7f84ecf6f6aaad467dcca5f8234b99
Zahra Janssens <zzahra.janssens@telenet.be>
2023-06-09 10:30:21 +0200

    feature: changed the login layout

--- login.php
+++ login.php
@@ -26,10 +26,7 @@
 ?>
 
 <header>
-<link rel="stylesheet" href="style.css"/>
-  <nav class="nav">
-    <a href="logout">Log out?</a>
-  </nav>    
+<link rel="stylesheet" href="style.css"/>  
 </header>
 
 <div id="app">
@@ -64,43 +61,6 @@
             <input type="text" id="email">
         </div>
     
-        <input type="submit" value="Log In" class="btn">
-        <!-- <a href="#" class="btn" id="btnSubmit">Log In</a> -->
+        <input type="submit" value="Inloggen" class="btn">
      </form>
 </div>
-
-
-
-
-
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css"/>
-    <title>login</title>
-</head>
-<body>
-    <img class="vormpje" src="./media/vorm.png" alt="vormpje">
-    
-    <img class="logo" src="./media/oceaanblauw.png" alt="Logo Conneco">
-
-    <form class="form-login" method="post" > <!- Form for logging in / HIER STOND EEN ACTION MET PHP CODE -
-       
-        <div class="input-login">
-          <label for="email">Email</label>
-          <input type="email" id="email" name="email" value="" required />
-
-          <label for="password">Wachtwoord</label>
-          <input type="password" id="password" name="password" required />
-        </div>
-
-        <a href="resetPassword.php"> Wachtwoord vergeten?</a>
-        <button type="submit" class="btn">Inloggen</button>
-
-        <a class="register" href="signup.php">Heb je nog geen account? Registreer je dan hier.</a>
-    </form>
-</body>
-</html>
