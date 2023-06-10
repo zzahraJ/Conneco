4e22e89c0864ce7312ca7eabb16f645edfefcd20
Zahra Janssens <zzahra.janssens@telenet.be>
2023-06-10 13:44:37 +0200

    feature: added the frontend of the skills page

--- skillsBewerkenUser.php
+++ skillsBewerkenUser.php
@@ -8,83 +8,93 @@
     <title>skills</title>
 </head>
     <body>
-        <nav class="search">
+
+    <div class="body-content">
+        <div class="sidebar">
+            <div class="image-container">
+                <img class="logo-wit" src="./media/logo-wit.png" alt="Logo Conneco">
+            </div>
+            
+            <div class="nav-container">
+                <a href="dashboardUser.php"><P>Dashboard</P></a> 
+                <a href="chat.php"><p>Chat</p></a>
+                <a href="profielUser.php"><p>Profiel</p></a>
+                <a href="badgesUser.php"><p>Badges</p></a>
+                <a href="instellingenUser.php"><p>Instellingen</p></a>
+            </div>
+        </div>
+        
+        <div class="main">
+            <div class="header search">
                 <div id="menu">
                     <a href="search.php">Zoeken</a>
                     <input type="text" name="search" placeholder="Zoeken">
                 </div>
-            </nav>
-
-        <div class="image-container">
-            <img class="row" src="./media/Blauwe-balk.png" alt="blauwe balk">
-            <img class="logo-wit" src="./media/logo-wit.png" alt="Logo Conneco">
+            </div>
             
+            <div class="content-aanduidenskills">
+                <div class="column-skillsaanduiden">
+                    <div class="eigen-projecten">
+                        <h2>Voeg skills toe aan jouw profiel.</h2>
+                    </div>
+                   
+                 
+                    <div class="skillsaanduiden">
+                        <div class="column-skillsaanduiden-container">
+                        
+                            <div class="column-skillsaanduiden">
+                                <input type="checkbox" id="myCheckbox">
+                                <label class="checkbox-label" for="myCheckbox">Project management</label>
+                            
+                                <input type="checkbox" id="myCheckbox">
+                                <label class="checkbox-label" for="myCheckbox">Klantvriendelijkheid</label>
+                            
+                                <input type="checkbox" id="myCheckbox">
+                                <label class="checkbox-label" for="myCheckbox">Financiële vaardigheden</label>
+                            
+                                <input type="checkbox" id="myCheckbox">
+                                <label class="checkbox-label" for="myCheckbox">Leiderschap vaardigheden</label>
+                            
+                                <input type="checkbox" id="myCheckbox">
+                                <label class="checkbox-label" for="myCheckbox">Analystische vaardigheden</label>
+                            
+                                <input type="checkbox" id="myCheckbox">
+                                <label class="checkbox-label" for="myCheckbox">Design vaardigheden</label>
+                            
+                                <input type="checkbox" id="myCheckbox">
+                                <label class="checkbox-label" for="myCheckbox">management vaardigheden</label>
+                            
+                                <input type="checkbox" id="myCheckbox">
+                                <label class="checkbox-label" for="myCheckbox">Communicatie vaardigheden</label>
+                            
+                                <input type="checkbox" id="myCheckbox">
+                                <label class="checkbox-label" for="myCheckbox">Business vaardigheden</label>
+                            
+                                <input type="checkbox" id="myCheckbox">
+                                <label class="checkbox-label" for="myCheckbox">Microsoft Office</label>
+                            
+                                <input type="checkbox" id="myCheckbox">
+                                <label class="checkbox-label" for="myCheckbox">Video vaardigheden</label>
+                            
+                                <input type="checkbox" id="myCheckbox">
+                                <label class="checkbox-label" for="myCheckbox">HR vaardigeheden</label>
+                            
+                                <input type="checkbox" id="myCheckbox">
+                                <label class="checkbox-label" for="myCheckbox">Administratieve vaardigheden</label>
+                            
+                                <input type="checkbox" id="myCheckbox">
+                                <label class="checkbox-label" for="myCheckbox">Bedrijfsstrategieën</label>
+                            
+                                <input type="checkbox" id="myCheckbox">
+                                <label class="checkbox-label" for="myCheckbox">Taalvaardigheden</label>
+                            
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="bewerken"><a href="skills.php">Opslaan</a></div>
         </div>
-        
-        <div class="nav-container">
-            <a href="dashboardUser.php"><P>Dashboard</P></a> 
-            <a href="chat.php"><p>Chat</p></a>
-            <a href="profielUser.php"><p>Profiel</p></a>
-            <a href="badgesUser.php"><p>Badges</p></a>
-            <a href="instellingenUser.php"><p>Instellingen</p></a>
-        </div>
-
-        <div class="omgedraaid-vormpje">
-            <img src="./media/vorm-omgedraaid.png" alt="vormpje">
-        </div>
-
-        <h2>Voeg skills toe aan het profiel</h2>
-
-        <div class="checkbox-container">
-            <input type="checkbox" id="myCheckbox">
-            <label class="checkbox-label" for="myCheckbox">Project management</label>
-
-            <input type="checkbox" id="myCheckbox">
-            <label class="checkbox-label" for="myCheckbox">Klantvriendelijkheid</label>
-
-            <input type="checkbox" id="myCheckbox">
-            <label class="checkbox-label" for="myCheckbox">Financiële vaardigheden</label>
-
-            <input type="checkbox" id="myCheckbox">
-            <label class="checkbox-label" for="myCheckbox">Leiderschap vaardigheden</label>
-
-            <input type="checkbox" id="myCheckbox">
-            <label class="checkbox-label" for="myCheckbox">Analystische vaardigheden</label>
-
-            <input type="checkbox" id="myCheckbox">
-            <label class="checkbox-label" for="myCheckbox">Design vaardigheden</label>
-
-            <input type="checkbox" id="myCheckbox">
-            <label class="checkbox-label" for="myCheckbox">management vaardigheden</label>
-
-            <input type="checkbox" id="myCheckbox">
-            <label class="checkbox-label" for="myCheckbox">Communicatie vaardigheden</label>
-
-            <input type="checkbox" id="myCheckbox">
-            <label class="checkbox-label" for="myCheckbox">Business vaardigheden</label>
-
-            <input type="checkbox" id="myCheckbox">
-            <label class="checkbox-label" for="myCheckbox">Microsoft Office</label>
-
-            <input type="checkbox" id="myCheckbox">
-            <label class="checkbox-label" for="myCheckbox">Video vaardigheden</label>
-
-            <input type="checkbox" id="myCheckbox">
-            <label class="checkbox-label" for="myCheckbox">HR vaardigeheden</label>
-
-            <input type="checkbox" id="myCheckbox">
-            <label class="checkbox-label" for="myCheckbox">Administratieve vaardigheden</label>
-
-            <input type="checkbox" id="myCheckbox">
-            <label class="checkbox-label" for="myCheckbox">Bedrijfsstrategieën</label>
-
-            <input type="checkbox" id="myCheckbox">
-            <label class="checkbox-label" for="myCheckbox">Taalvaardigheden</label>
-        </div>
-
-        <div class="bewerken"><a href="skills.php">Opslaan</a></div>
-
-
-        
+    </div>   
     </body>
 </html>
