1fedcd65bf6cfd1674e920ad69ccadaa6a028cbe
Zahra Janssens <zzahra.janssens@telenet.be>
2023-06-12 18:18:20 +0200

    feature: fixed the profile page for the Admin

---
+++ skillsAdmin.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css"/>
+    <title>skills</title>
+</head>
+    <body>
+    <div class="body-content">
+        <div class="sidebar">
+            <div class="image-container">
+                <img class="logo-wit" src="./media/logo-wit.png" alt="Logo Conneco">
+            </div>
+            
+            <div class="nav-container">
+                <a href="dashboard.php"><P>Dashboard</P></a> 
+                <a href="werknemers.php"><p>Werknemers</p></a>
+                <a href="categoriën.php"><p>Categoriën</p></a>
+                <a href="profiel.php"><p>Profiel</p></a>
+                <a href="badges.php"><p>Badges</p></a>
+                <a href="instellingen.php"><p>Instellingen</p></a>
+            </div>
+        </div>
+        
+        <div class="main">
+            <div class="header search">
+                <div id="menuutje">
+                    <a href="searchAdmin.php"><input></a>
+                </div>
+            </div>
+            
+            <div class="skills">
+                <h2>Skills</h2>
+                <div class="column-skills-container">
+                    <div class="column-skills">
+                        <h3>Microsoft Office</h3>
+                        <h3>Administratieve vaardigheden</h3>
+                        <h3>Taalvaardigheid</h3>
+                        <h3>Klantvriedenlijkheid</h3>
+                    </div>
+                    <div class="column-skills">
+                        <p class="skills-image"><img src="./media/balkje-4.png" alt="blakje skill"></p>
+                        <p class="skills-image"><img src="./media/balkje-4.png" alt="blakje skill"></p>
+                        <p class="skills-image"><img src="./media/balkje-3.png" alt="blakje skill"></p>
+                        <p class="skills-image"><img src="./media/balkje-3.png" alt="blakje skill"></p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="bewerken"><a href="skillsBewerkenAdmin.php">Bewerken</a></div>
+            <div class="profielfoto"><img src="./media/julie.png" alt="profielfoto"></div> 
+        </div>
+    </div>
+        
+    </body>
+</html>
---
+++ skillsBewerkenAdmin.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css"/>
+    <title>skills</title>
+</head>
+    <body>
+
+    <div class="body-content">
+        <div class="sidebar">
+            <div class="image-container">
+                <img class="logo-wit" src="./media/logo-wit.png" alt="Logo Conneco">
+            </div>
+            
+            <div class="nav-container">
+                <a href="dashboard.php"><P>Dashboard</P></a> 
+                <a href="werknemers.php"><p>Werknemers</p></a>
+                <a href="categoriën.php"><p>Categoriën</p></a>
+                <a href="profiel.php"><p>Profiel</p></a>
+                <a href="badges.php"><p>Badges</p></a>
+                <a href="instellingen.php"><p>Instellingen</p></a>
+            </div>
+        </div>
+        
+        <div class="main">
+            <div class="header search">
+                <div id="menuutje">
+                    <a href="searchAdmin.php"><input></a>
+                </div>
+            </div>
+            
+            <div class="content-aanduidenskills">
+                <div class="column-skillsaanduiden">
+                    <div class="eigen-projecten">
+                        <h2>Voeg skills toe aan jouw profiel.</h2>
+                    </div>
+                    
+                    
+                    <div class="skillsaanduiden">
+                        <div class="column-skillsaanduiden-container">
+                            
+                            <div class="column-skillsaanduiden">
+                                <input type="checkbox" id="myCheckbox">
+                                <label class="text">Project management</label>
+                                
+                                <input type="checkbox" id="myCheckbox">
+                                <label class="text">Klantvriendelijkheid</label>
+                                
+                                <input type="checkbox" id="myCheckbox">
+                                <label class="text">Financiële vaardigheden</label>
+                                
+                                <input type="checkbox" id="myCheckbox">
+                                <label class="text">Leiderschap vaardigheden</label>
+                                
+                                <input type="checkbox" id="myCheckbox">
+                                <label class="text">Analystische vaardigheden</label>
+                                
+                                <input type="checkbox" id="myCheckbox">
+                                <label class="text">Design vaardigheden</label>
+                                
+                                <input type="checkbox" id="myCheckbox">
+                                <label class="text">management vaardigheden</label>
+                                
+                                <input type="checkbox" id="myCheckbox">
+                                <label class="text">Communicatie vaardigheden</label>
+                                
+                                <input type="checkbox" id="myCheckbox">
+                                <label class="text">Business vaardigheden</label>
+                                
+                                <input type="checkbox" id="myCheckbox">
+                                <label class="text">Microsoft Office</label>
+                                
+                                <input type="checkbox" id="myCheckbox">
+                                <label class="text">Video vaardigheden</label>
+                                
+                                <input type="checkbox" id="myCheckbox">
+                                <label class="text">HR vaardigeheden</label>
+                                
+                                <input type="checkbox" id="myCheckbox">
+                                <label class="text">Administratieve vaardigheden</label>
+                                
+                                <input type="checkbox" id="myCheckbox">
+                                <label class="text">Bedrijfsstrategieën</label>
+                                
+                                <input type="checkbox" id="myCheckbox">
+                                <label class="text">Taalvaardigheden</label>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="bewerken"><a href="profiel.php">Opslaan</a></div>
+        </div>
+    </div>   
+</body>
+</html>
