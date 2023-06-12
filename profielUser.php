dca16951261467d1af19a78a29de463323f96dfa
Zahra Janssens <zzahra.janssens@telenet.be>
2023-06-12 06:59:05 +0200

    feature: changed the frontend of the user profile page

--- profielUser.php
+++ profielUser.php
@@ -50,7 +50,7 @@
                             <input type="text" id="bio" name="bio" value="" required />
                         </div>
                             
-                    <div class="column-profile">    
+                    <div class="column-profile"> 
                         <div class="tags">
                             <h3>Tags</h3>
                             <p>Klantvriendelijkheid</p>
@@ -59,95 +59,21 @@
                         </div>
                         
                         <div class="certificaten">
-                            <h2>Certificaten</h2>
-                            <div class="column-profiel-container">
-                                <div class="column-profiel">
-                                    <p class="gebruikers-image"><img src="./media/puntje.png" alt="puntje"></p>
-                                    <p class="gebruikers-image"><img src="./media/puntje.png" alt="puntje"></p>
-                                    <p class="gebruikers-image"><img src="./media/puntje.png" alt="puntje"></p>
-                                </div>
-                                <div class="column-profiel">
-                                    <p>Bachelor Communicatie</p>
-                                    <p>Workshop Communicatie</p>
-                                    <p>Bijscholing Microsoft Office</p>
+                            <h3>Certificaten</h3>
+                            <div class="column-users-container">
+                                <div class="column-users">
+                                    <p class="gebruikers-image"><img src="./media/puntje.png" alt="puntje">Bachelor Bedrijfskunde</p>
+                                    <p class="gebruikers-image"><img src="./media/puntje.png" alt="puntje">Workshop Communicatie</p>
+                                    <p class="gebruikers-image"><img src="./media/puntje.png" alt="puntje">Bijscholing Microsoft Office</p>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="foto"><img src="./media/julie.png" alt="profielfoto"></div>
-                <div class="Btn"><a href="profielBewerkenUser.php">Bewerken</a></div>
-                <div class="Btn"><a href="skills.php">Mijn skills</a></div>
             </div>
+            <div class="foto"><img src="./media/julie.png" alt="profielfoto"></div>
+            <div class="Btn"><a href="profielBewerkenUser.php">Bewerken</a></div>
+            <div class="Btn"><a href="skills.php">Mijn skills</a></div>
         </div>
     </body>
-
-
-
-
-   
-    <!-- <body>
-        <div class="body-content">
-            <div class="sidebar">
-                <div class="image-container">
-                    <img class="logo-wit" src="./media/logo-wit.png" alt="Logo Conneco">
-                </div>
-                
-                <div class="nav-container">
-                    <a href="dashboardUser.php"><P>Dashboard</P></a> 
-                    <a href="chat.php"><p>Chat</p></a>
-                    <a href="profielUser.php"><p>Profiel</p></a>
-                    <a href="badgesUser.php"><p>Badges</p></a>
-                    <a href="instellingenUser.php"><p>Instellingen</p></a>
-                </div>
-            </div>
-            
-            <div class="main">
-                <div class="header search">
-                    <div id="menu">
-                        <a href="search.php">Zoeken</a>
-                        <input type="text" name="search" placeholder="Zoeken">
-                    </div>
-                </div>
-                <div class="profielfoto"><img src="./media/julie.png" alt="profielfoto"></div>
-             
-                <div class="content-instellingen">
-                    <div class="column-instellingen">
-                        <div class="column-users-container">
-                            <div class="column-users">
-                            <h3>Naam<h3>
-                                <input type="text" id="voornaam" name="voornaam" value="" required />
-                        
-                                <h3>Functie<h3>
-                                <input type="text" id="functie" name="functie" value="" required />
-                        
-                                <h3>Afdeling<h3>
-                                <input type="text" id="afdeling" name="afdeling" value="" required />
-                        
-                                <h3>In dienst sinds<h3>
-                                <input type="text" id="inDienst" name="inDienst" value="" required />
-
-                                <h3>Bio<h3>
-                                <input type="text" id="bio" name="bio" value="" required />
-                            </div>
-
-                            
-                            <div class="column-users">
-                                <h3>Tags</h3>
-                                <p>Klantvriendelijkheid</p>
-                            </div>
-                            
-                            <div class="column-users">
-                                <h3>Certificaten</h3>
-                                <p>Klantvriendelijkheid</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="profielfoto"><img src="./media/julie.png" alt="profielfoto"></div>
-                <div class="Btn"><a href="profielBewerkenUser.php">Bewerken</a></div>
-                <div class="Btn"><a href="skills.php">Mijn skills</a></div>
-            </div>
-        </div>
-    </body> -->
 </html>
