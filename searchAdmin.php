e169b37e57afbea64975c27095e49e13205bca71
Zahra Janssens <zzahra.janssens@telenet.be>
2023-06-13 14:14:50 +0200

    feature: fixed the search bar

---
+++ searchAdmin.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css"/>
+    <title>Zoeken</title>
+</head>
+<body>
+    <div class="body-content">
+        <div class="sidebar">
+            <div class="image-container">
+                <img class="logo-wit" src="./media/logo-wit.png" alt="Logo Conneco">
+            </div>
+            
+            <div class="nav-container">
+                <a href="dashboard.php"><P>Dashboard</P></a> 
+                <a href="werknemers.php"><p>Werknemers</p></a>
+                <a href="categoriën.php"><p>Categorieën</p></a>
+                <a href="profiel.php"><p>Profiel</p></a>
+                <a href="badges.php"><p>Badges</p></a>
+                <a href="instellingen.php"><p>Instellingen</p></a>
+            </div>
+        </div>
+        
+        <div class="main">
+        <div class="header search">
+                        <div id="menuutje">
+                            <a href="searchAdmin.php"><input></a>
+                        </div>
+                    </div>
+        
+            <div class="content">
+                <div class="column">
+                    <a href="profielcollega.php">
+                        <div class="project"> 
+                            <h3>Thomas Peeters</h3>
+                            <p>Digital Strategist</p>
+                            <p>J3 bureau 12</p>
+                            <p>Microsoft Office</p>
+                            <p>Adobe Illustrator</p>
+                            <p>Taalvaardigheid</p>
+                        </div>
+                    </a>
+
+                    <div class="project"> 
+                        <h3>Thomas Janssens</h3>
+                        <p>HR</p>
+                        <p>Q1 bureau 6</p>
+                        <p>Klantvriendelijkheid</p>
+                        <p>Taalvaardigheid</p>
+                    </div>
+
+                    <div class="project"> 
+                        <h3>Thomas Vermeulen</h3>
+                        <p>Administratief medewerker</p>
+                        <p>Q5 bureau 3</p>
+                        <p>Microsoft Office</p>
+                        <p>Administratieve vaardigheden</p>
+                    </div>
+                </div>
+                <div class="column">
+                    <div class="deadlines">
+                        <h3>Taalvaardigheid</h3>
+                    </div>
+
+                    <div class="deadlines">
+                        <h3>Development</h3>
+                    </div>
+
+                    <div class="deadlines">
+                        <h3>HR</h3>
+                    </div>
+
+                    <div class="deadlines">
+                        <h3>Administratief</h3>
+                    </div>
+
+                    <div class="deadlines">
+                        <h3>Business vaardigheden</h3>
+                    </div>
+
+                    <div class="deadlines">
+                        <h3>Project management</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </body>
+    </html>
