ee8be0e6c504c3c15ed1a248fe97694081f8e032
Zahra Janssens <zzahra.janssens@telenet.be>
2023-06-13 15:55:14 +0200

    feature: added the werknemers page

--- werknemers.php
+++ werknemers.php
@@ -8,32 +8,126 @@
     <title>Werknemers</title>
 </head>
     <body>
-           
-        <nav class="search">
-            <div id="menu">
-                <a href="search.php">Zoeken</a>
-                <input type="text" name="search" placeholder="Zoeken">
+        <div class="body-content">
+            <div class="sidebar">
+                <div class="image-container">
+                    <img class="logo-wit" src="./media/logo-wit.png" alt="Logo Conneco">
+                </div>
+                
+                <div class="nav-container">
+                    <a href="dashboard.php"><P>Dashboard</P></a> 
+                    <a href="werknemers.php"><p>Werknemers</p></a>
+                    <a href="categoriën.php"><p>Categorieën</p></a>
+                    <a href="profiel.php"><p>Profiel</p></a>
+                    <a href="badges.php"><p>Badges</p></a>
+                    <a href="instellingen.php"><p>Instellingen</p></a>
+                </div>
             </div>
-        </nav>
-
-        <div class="image-container">
-            <img class="row" src="./media/Blauwe-balk.png" alt="blauwe balk">
-            <img class="logo-wit" src="./media/logo-wit.png" alt="Logo Conneco">
-        </div>
-
-        <div class="nav-container">
-            <a href="dashboard.php"><P>Dashboard</P></a> 
-            <a href="werknemers.php"><p>Werknemers</p></a>
-            <a href="categorieën.php"><p>Categorieën</p></a>
-            <a href="badges.php"><p>Badges</p></a>
-            <a href="profiel.php"><p>Profiel</p></a>
-            <a href="instellingen.php"><p>Instellingen</p></a>
-        </div>
-
+            
+        <div class="main">
+            <div class="header search">
+                <div id="menu">
+                    <a href="searchAdmin.php"><input></a>
+                </div>
+            </div>
         
+            <div class="content">
+                <div class="column">
+                    <div class="column">
+                        <div class="eigen-projecten">
+                            <h2>Werknemers</h2>
+                        </div>
+                        
+                        <div class="werknemers">
+                            <h3>Thomas Peeters</h3>
+                            <p>Administratief Medewerker</p>
+                            <p>Afdeling Q7 bureau 5</p>
+                            <p>Bachelor Bedrijfsmanagement</p>
+                            <p>Actief</p>
+                        </div>
 
+                        <div class="werknemers">
+                            <h3>Sarah Verbot</h3>
+                            <p>Administratief Medewerker</p>
+                            <p>Afdeling Q4 bureau 2</p>
+                            <p>Bachelor Logistics</p>
+                            <p>Actief</p>
+                        </div>
 
-   
-    
+                        <div class="werknemers">
+                            <h3>Ruben Vermaelen</h3>
+                            <p>HR medewerker</p>
+                            <p>Afdeling Q6 bureau 9</p>
+                            <p>Bachelor Digital Marketing</p>
+                            <p>Actief</p>
+                        </div>
+
+                        <div class="werknemers">
+                            <h3>Lisa Gevers</h3>
+                            <p>Medewerker Kredieten</p>
+                            <p>Afdeling Q8 bureau 2</p>
+                            <p>Bachelor Bedrijfsmanagement</p>
+                            <p>Actief</p>
+                        </div>
+
+                        <div class="werknemers">
+                            <h3>Laura Gillis</h3>
+                            <p>Administratief Medewerker</p>
+                            <p>Afdeling Q4 bureau 1</p>
+                            <p>Bachelor Bedrijfsmanagement</p>
+                            <p>Actief</p>
+                        </div>
+
+                        <div class="werknemers">
+                            <h3>Nancy Maris</h3>
+                            <p>Projectmanager</p>
+                            <p>Afdeling Q3 bureau 8</p>
+                            <p>Bachelor Business Management</p>
+                            <p>Actief</p>
+                        </div>
+
+                        <div class="werknemers">
+                            <h3>Pegie Torfs</h3>
+                            <p>HR medewerker</p>
+                            <p>Afdeling Q6 bureau 5</p>
+                            <p>Bachelor Management</p>
+                            <p>Actief</p>
+                        </div>
+
+                        <div class="werknemers">
+                            <h3>Lucas Janssens</h3>
+                            <p>Medewerker Kredieten</p>
+                            <p>Afdeling Q8 bureau 4</p>
+                            <p>Bachelor Bedrijfsmanagement</p>
+                            <p>Actief</p>
+                        </div>
+
+                        <div class="werknemers">
+                            <h3>Tibo Claes</h3>
+                            <p>Administratief Medewerker</p>
+                            <p>Afdeling Q4 bureau 5</p>
+                            <p>Bachelor Ditgital Media</p>
+                            <p>Actief</p>
+                        </div>
+
+                        <div class="werknemers">
+                            <h3>Olivier Jacobs</h3>
+                            <p>HR Medewerker</p>
+                            <p>Afdeling Q6 bureau 7</p>
+                            <p>Bachelor Management</p>
+                            <p>Actief</p>
+                        </div>
+
+                        <div class="werknemers">
+                            <h3>Bram Smets</h3>
+                            <p>Medewerker Kredieten</p>
+                            <p>Afdeling Q8 bureau 5</p>
+                            <p>Bachelor Economie</p>
+                            <p>Actief</p>
+                        </div>  
+                    </div>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
