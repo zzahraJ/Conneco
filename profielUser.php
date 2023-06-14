f9e6d2c28a1ddc1fd2d4fc60d84d3f3067860f65
Zahra Janssens <zzahra.janssens@telenet.be>
2023-06-14 13:11:49 +0200

    feature: fixed the profile page

--- profielUser.php
+++ profielUser.php
@@ -5,9 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css"/>
-    <title>profiel</title>
+    <title>profielBewerken</title>
 </head>
-    <body>
+    <body>  
+
         <div class="body-content">
             <div class="sidebar">
                 <div class="image-container">
@@ -30,49 +31,48 @@
                     </div>
                 </div>
                 
+                <!-- hier moet php code in om de naam van het project weer te geven -->
+            
                 <div class="content-instellingen">
                     <div class="column-instellingen">
-                        <div class="profiel-info"> 
-                            <h3>Naam</h3>
-                            <input type="text" id="voornaam" name="voornaam" value="" required />
+                        <h3>Voornaam<h3>
+                        <input type="text" id="voornaam" name="voornaam" value="" required />
+                
+                        <h3>Functie<h3>
+                        <input type="text" id="funtie" name="funtie" value="" required />
 
-                            <h3>Functie</h3>
-                            <input type="text" id="functie" name="functie" value="" required />
+                        <h3>Afdeling<h3>
+                        <input type="text" id="afdeling" name="afdeling" value="" required />
+                
+                        <h3>In dienst sinds<h3>
+                        <input type="text" id="inDienst" name="inDienst" value="" required />
+                
+                        <h3>Bio<h3>
+                        <input type="text" id="bio" name="bio" value="" required />
+                    </div>
+                </div>
+                
+                <div class="profielf"><img src="./media/julie.png" alt="profielfoto"></div>
+                <div class="btns"><a href="profielBewerkenUser.php">Bewerken</a></div>
+                <div class="button"><a href="skills.php">Mijn skills</a></div>
 
-                            <h3>Afdeling</h3>
-                            <input type="text" id="afdeling" name="afdeling" value="" required />
-
-                            <h3>In dienst sinds</h3>
-                            <input type="text" id="inDienst" name="inDienst" value="" required />
-
-                            <h3>Bio</h3>
-                            <input type="text" id="bio" name="bio" value="" required />
-                        </div>
-                            
-                    <div class="column-profile"> 
-                        <div class="tags">
-                            <h3>Tags</h3>
-                            <p>Klantvriendelijkheid</p>
-                            <p>Administratief</p>
-                            <p>Microsoft Office</p>
-                        </div>
-                        
-                        <div class="certificaten">
-                            <h3>Certificaten</h3>
-                            <div class="column-users-container">
-                                <div class="column-users">
-                                    <p class="gebruikers-image"><img src="./media/puntje.png" alt="puntje">Bachelor Bedrijfskunde</p>
-                                    <p class="gebruikers-image"><img src="./media/puntje.png" alt="puntje">Workshop Communicatie</p>
-                                    <p class="gebruikers-image"><img src="./media/puntje.png" alt="puntje">Bijscholing Microsoft Office</p>
-                                </div>
-                            </div>
-                        </div>
+                <div class="column-profiel">
+                    <div class="tags">
+                        <h3>Tags</h3>
+                        <p>Klantvriendelijkheid</p>
+                        <p>Administratief</p>
+                        <p>Microsoft Office</p>
+                        <p>Taalvaardigheid</p>
+                    </div>
+                    
+                    <div class="certificaten">
+                        <h3>Certificaten</h3>
+                        <p>Bachelor Bedrijfskunde</p>
+                        <p>Workshop Communicatie</p>
+                        <p>Bijscholing Microsoft Office</p>
                     </div>
                 </div>
             </div>
-            <div class="foto"><img src="./media/julie.png" alt="profielfoto"></div>
-            <div class="Btn"><a href="profielBewerkenUser.php">Bewerken</a></div>
-            <div class="Btn"><a href="skills.php">Mijn skills</a></div>
         </div>
     </body>
 </html>
